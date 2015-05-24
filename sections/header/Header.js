3da69f0f96b03a8d978bae38c5ed1ff81390a9f9
Markel Arizaga <markel.arizaga@gmail.com>
2015-05-24 18:50:04 +0200

    Adding the offline/online icons to the header (temporary icons)

--- sections/header/Header.js
+++ sections/header/Header.js
@@ -1,5 +1,8 @@
 function Header($scope, sectionNavigator, feedsCache, backendService, networkStatusService, syncService) {
+	
 	$scope.isRoot = true;
+	$scope.offline = false;
+
 	sectionNavigator.addEventListener("onSectionChanged", function(newSection){
 		var id = newSection.split("/");
 		id = id[id.length-1];
@@ -16,11 +19,13 @@ function Header($scope, sectionNavigator, feedsCache, backendService, networkSta
 		if(!networkStatusService.isOfflineMode()) {
 			backendService.goOffline(function (feeds) {
 				alert("Offline mode is ready");
+				$scope.offline = true;
 				feedsCache.setOfflineFeeds(feeds);
 				networkStatusService.setOfflineMode(true);
 				sectionNavigator.navigateTo(sectionNavigator.section.ROOT_SECTION);
 			});
 		} else {
+			$scope.offline = false;
 			networkStatusService.setOfflineMode(false);
 			feedsCache.clear();
 			syncService.syncArticlesInServer();
