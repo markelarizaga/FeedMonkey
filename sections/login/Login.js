539854b60e2937dc2fcb1645b822b6073f281c35
Markel Arizaga <markel.arizaga@gmail.com>
2015-08-12 18:13:54 +0200

    Now when the login is tried the activity indicator is shown

--- sections/login/Login.js
+++ sections/login/Login.js
@@ -1,15 +1,18 @@
 angular.module('TinyRSS').
-controller('Login', ['$scope', 'sectionNavigator', 'authenticationService', 'settingsService', '$filter',
-function ($scope, sectionNavigator, authenticationService, settingsService, $filter) {
+controller('Login', ['$scope', 'sectionNavigator', 'authenticationService', 'settingsService', '$filter', 'backgroundActivityService',
+function ($scope, sectionNavigator, authenticationService, settingsService, $filter, backgroundActivityService) {
 
 	$scope.attemptLogin = function() {
 		var login = null;
 		if ($scope.username && $scope.password && $scope.server_url) {
+			backgroundActivityService.notifyBackgroundActivity();
 			settingsService.setCredentials($scope.server_url, $scope.username, $scope.password);
 			login = authenticationService.login($scope.server_url, $scope.username, $scope.password);
 			login.then(function(){
+				backgroundActivityService.notifyBackgroundActivityStopped();
 				sectionNavigator.navigateTo(sectionNavigator.section.CATEGORIES);
 			}, function(error) {
+				backgroundActivityService.notifyBackgroundActivityStopped();
 				var errorMessage = $filter('translate')('loginError');
 				var errorMessage = error ? errorMessage + ': ' + $filter('translate')(error) : errorMessage;
 				alert(errorMessage);
