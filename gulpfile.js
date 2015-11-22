beff31b58ba6c3532495a9d0793a62aca4734612
Markel Arizaga <markel.arizaga@gmail.com>
2015-11-23 00:01:28 +0100

    First approach to a gulp based release build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var zip = require('gulp-zip');
 
-gulp.task('default', function() {
-  gulp.src(['*/*.css', 'sections/*', 'services/*', 'locales/*', 'lib/*', 'img/*'])
+gulp.task('delete-dist', function(){
+  return gulp.src('dist')
+    .pipe(clean());
+});
+
+gulp.task('default', ['delete-dist'], function() {
+  gulp.src(['*/**/*.css',
+            '**/*.js',
+            '**/*.html',
+            '*/**/*.png',
+            '*/**/*.svg',
+            'manifest.webapp',
+            '!node_modules/**',
+            '!gulpfile.js'])
+  .pipe(zip('ttrss.zip'))
   .pipe(gulp.dest('dist/'));
 });
