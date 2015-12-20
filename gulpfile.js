be96b21c737c56e119247e27b55c71444305e5d7
Markel Arizaga <markel.arizaga@gmail.com>
2015-12-21 00:30:06 +0100

    Now there is a gulp task that creates a zip file of the app in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,35 @@ var zip = require('gulp-zip');
 
 gulp.task('delete-dist', function(){
   return gulp.src('dist')
-    .pipe(clean());
+        .pipe(clean());
 });
 
-gulp.task('default', ['delete-dist'], function() {
-  gulp.src(['*/**/*.css',
+gulp.task('move-to-dist', ['delete-dist'], function() {
+  return gulp.src([
+            '*/**/*.css',
             '**/*.js',
             '**/*.html',
             '*/**/*.png',
             '*/**/*.svg',
             'manifest.webapp',
             '!node_modules/**',
+            '!bower_components/**',
             '!gulpfile.js'])
-  .pipe(zip('ttrss.zip'))
-  .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('move-dependencies', ['move-to-dist'], function(){
+  return gulp.src([
+            'bower_components/**/*.min.js'])
+        .pipe(gulp.dest('dist/bower_components/'));
+});
+
+gulp.task('default', [
+        'move-to-dist',
+        'move-dependencies'
+      ],
+    function(){
+      return gulp.src('dist/**')
+            .pipe(zip('ttrss.zip'))
+            .pipe(gulp.dest('dist/'));
+    });
